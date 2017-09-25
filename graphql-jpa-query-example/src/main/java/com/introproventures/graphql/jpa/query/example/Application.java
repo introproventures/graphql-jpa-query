b204ec564d4d3bd8b9aa6fee108f49969f7fbd9a
Igor Dianov <igor@dianov.com>
2017-09-25 14:33:02 -0700

    Renamed graphiql.html to index.html to resolve / request

--- graphql-jpa-query-example/src/main/java/com/introproventures/graphql/jpa/query/example/Application.java
+++ graphql-jpa-query-example/src/main/java/com/introproventures/graphql/jpa/query/example/Application.java
@@ -3,24 +3,19 @@ package com.introproventures.graphql.jpa.query.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * GraphQL JPA Query Example with Spring Boot Autoconfiguration
+ * 
+ * You can configure GraphQL JPA Query properties in application.yml 
+ * 
+ * @author Igor Dianov
+ *
+ */
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
-//    @Bean
-//    public GraphQLExecutor graphQLExecutor(final GraphQLSchemaBuilder graphQLSchemaBuilder) {
-//        return new GraphQLJpaExecutor(graphQLSchemaBuilder.build());
-//    }
-//
-//    @Bean
-//    public GraphQLSchemaBuilder graphQLSchemaBuilder(final EntityManager entityManager) {
-//        
-//        return new GraphQLJpaSchemaBuilder(entityManager)
-//            .name("GrapQLBooks")
-//            .description("Books JPA Test Schema");
-//    }
     
 }
