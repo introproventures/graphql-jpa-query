cb4d16a4500fcae2878e2790bd7aaabeb8e72fdb
molexx <molexx@users.noreply.github.com>
2019-03-15 00:57:55 +0000

    restore DroidFunction into starwars package

---
+++ graphql-jpa-query-example/src/main/java/com/introproventures/graphql/jpa/query/example/starwars/DroidFunction.java
@@ -0,0 +1,25 @@
+package com.introproventures.graphql.jpa.query.example.starwars;
+
+import com.introproventures.graphql.jpa.query.annotation.GraphQLDescription;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity(name = "droid_function")
+@GraphQLDescription("Represents the functions a droid can have")
+@Data
+@EqualsAndHashCode()
+public class DroidFunction {
+
+    @Id
+    @GraphQLDescription("Primary Key for the DroidFunction Class")
+    String id;
+    
+    String function;
+    
+    
+
+}
