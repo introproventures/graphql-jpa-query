15771a092bd72f783346217c6cd40dde12e319c3
Igor Dianov <igor@dianov.com>
2019-06-09 19:14:31 -0700

    feat: add correlation subquery testers

--- graphql-jpa-query-schema/src/test/java/com/introproventures/graphql/jpa/query/converter/GraphQLJpaConverterTests.java
+++ graphql-jpa-query-schema/src/test/java/com/introproventures/graphql/jpa/query/converter/GraphQLJpaConverterTests.java
@@ -26,6 +26,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Subquery;
@@ -169,6 +170,66 @@ public class GraphQLJpaConverterTests {
         // then:
         assertThat(result).isNotEmpty();
         assertThat(result).hasSize(1);
+    }    
+    
+    @Test
+    @Transactional
+    public void criteriaTester4() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<TaskEntity> tasksQuery = cb.createQuery(TaskEntity.class);
+        Root<TaskEntity> task = tasksQuery.from(TaskEntity.class);
+        
+        Subquery<TaskVariableEntity> subquery = tasksQuery.subquery(TaskVariableEntity.class);
+        Root<TaskEntity> taskCorrelation = subquery.correlate(task);
+        
+        Join<TaskEntity, TaskVariableEntity> taskVariables = taskCorrelation.join("variables");
+        
+        Predicate var1 = cb.and(cb.equal(taskVariables.get("name"), "variable2"), 
+                                cb.equal(taskVariables.get("value"), new VariableValue<>(new Boolean(true))));
+
+        Predicate var2 = cb.and(cb.equal(taskVariables.get("name"), "variable1"), 
+                                cb.equal(taskVariables.get("value"), new VariableValue<>(new String("data"))));
+        
+        tasksQuery.select(task)
+                  .where(cb.and(cb.exists(subquery.select(taskVariables).where(var1)),
+                                cb.exists(subquery.select(taskVariables).where(var2))));
+        // when:
+        List<TaskEntity> result = entityManager.createQuery(tasksQuery).getResultList();
+
+        // then:
+        assertThat(result).isNotEmpty();
+        assertThat(result).hasSize(1);
+    }
+    
+    @Test
+    @Transactional
+    public void criteriaTester5() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<TaskEntity> tasksQuery = cb.createQuery(TaskEntity.class);
+        Root<TaskEntity> task = tasksQuery.from(TaskEntity.class);
+        
+        Subquery<TaskVariableEntity> subquery = tasksQuery.subquery(TaskVariableEntity.class);
+        Root<TaskEntity> taskCorrelation = subquery.correlate(task);
+        
+        Join<TaskEntity, TaskVariableEntity> taskVariables = taskCorrelation.join("variables");
+        
+        Predicate var1 = cb.and(cb.equal(taskVariables.get("name"), "variable2"), 
+                                cb.equal(taskVariables.get("value"), new VariableValue<>(new Boolean(true))));
+
+        Predicate var2 = cb.and(cb.equal(taskVariables.get("name"), "variable1"), 
+                                cb.equal(taskVariables.get("value"), new VariableValue<>(new String("data"))));
+        
+        tasksQuery.select(task)
+                  .where(cb.not(cb.and(cb.exists(subquery.select(taskVariables).where(var1)),
+                                cb.exists(subquery.select(taskVariables).where(var2)))));
+        // when:
+        List<TaskEntity> result = entityManager.createQuery(tasksQuery).getResultList();
+
+        // then:
+        assertThat(result).isNotEmpty();
+        assertThat(result).hasSize(4);
     }    
     
     @Test // Problem with generating cast() in the where expression
