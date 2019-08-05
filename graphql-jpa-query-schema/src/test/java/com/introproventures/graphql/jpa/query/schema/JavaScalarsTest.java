6d4babc8a9c90096268b489ad4a6c88c9a6b1cc6
Sinai <sinainin.sn@gmail.com>
2019-08-05 15:36:10 +0700

    Fix format

--- graphql-jpa-query-schema/src/test/java/com/introproventures/graphql/jpa/query/schema/JavaScalarsTest.java
+++ graphql-jpa-query-schema/src/test/java/com/introproventures/graphql/jpa/query/schema/JavaScalarsTest.java
@@ -16,6 +16,9 @@
 
 package com.introproventures.graphql.jpa.query.schema;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
 import java.time.*;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -29,14 +32,12 @@ import graphql.schema.CoercingSerializeException;
 import graphql.schema.GraphQLScalarType;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
-
 public class JavaScalarsTest {
 
     @Test
     public void long2LocalDateTime() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalDateTime.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalDateTime.class).getCoercing();
 
         LocalDateTime localDateTime = LocalDateTime.of(2017, 02, 02, 12, 30, 15);
         long input = localDateTime.toEpochSecond(ZoneId.systemDefault().getRules().getOffset(localDateTime));
@@ -48,7 +49,7 @@ public class JavaScalarsTest {
         assertThat(result).isInstanceOf(LocalDateTime.class);
 
         LocalDateTime resultLDT = (LocalDateTime) result;
-
+        
         assert resultLDT.getDayOfMonth() == 2;
         assert resultLDT.getMonth() == Month.FEBRUARY;
         assert resultLDT.getYear() == 2017;
@@ -60,7 +61,7 @@ public class JavaScalarsTest {
     @Test
     public void string2LocalDateTime() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalDateTime.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalDateTime.class).getCoercing();
         final String input = "2017-02-02T12:30:15";
 
         //when
@@ -70,7 +71,7 @@ public class JavaScalarsTest {
         assertThat(result).isInstanceOf(LocalDateTime.class);
 
         LocalDateTime resultLDT = (LocalDateTime) result;
-
+        
         assert resultLDT.getDayOfMonth() == 2;
         assert resultLDT.getMonth() == Month.FEBRUARY;
         assert resultLDT.getYear() == 2017;
@@ -82,7 +83,7 @@ public class JavaScalarsTest {
     @Test
     public void long2LocalDate() {
         // given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalDate.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalDate.class).getCoercing();
         LocalDateTime localDateTime = LocalDateTime.of(2017, 02, 02, 0, 0, 0);
         long input = localDateTime.toEpochSecond(ZoneId.systemDefault().getRules().getOffset(localDateTime));
 
@@ -93,7 +94,7 @@ public class JavaScalarsTest {
         assertThat(result).isInstanceOf(LocalDate.class);
 
         LocalDate resultLDT = (LocalDate) result;
-
+        
         assert resultLDT.getDayOfMonth() == 2;
         assert resultLDT.getMonth() == Month.FEBRUARY;
         assert resultLDT.getYear() == 2017;
@@ -102,7 +103,7 @@ public class JavaScalarsTest {
     @Test
     public void string2LocalDate() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalDate.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalDate.class).getCoercing();
         final String input = "2017-02-02";
 
         //when
@@ -112,16 +113,16 @@ public class JavaScalarsTest {
         assertThat(result).isInstanceOf(LocalDate.class);
 
         LocalDate resultLDT = (LocalDate) result;
-
+        
         assert resultLDT.getDayOfMonth() == 2;
         assert resultLDT.getMonth() == Month.FEBRUARY;
         assert resultLDT.getYear() == 2017;
     }
-
+    
     @Test
     public void testLocalTimeParseLiteralValue() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
 
         //then
         assertThat(coercing.parseLiteral(new StringValue("00:00:00"))).isEqualTo(LocalTime.MIDNIGHT);
@@ -129,11 +130,11 @@ public class JavaScalarsTest {
         assertThat(coercing.parseLiteral(new StringValue("17:59:59"))).isEqualTo(LocalTime.of(17, 59, 59));
         assertThat(coercing.parseLiteral(new StringValue("not a time"))).isNull();
     }
-
+    
     @Test
     public void testLocalTimeSerializeValue() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
 
         //then
         assertThat(coercing.serialize(LocalTime.MIDNIGHT)).isEqualTo("00:00:00");
@@ -141,24 +142,24 @@ public class JavaScalarsTest {
         assertThat(coercing.serialize(LocalTime.of(17, 59, 59))).isEqualTo("17:59:59");
         assertThat(coercing.serialize(LocalTime.of(17, 59, 59, (int) TimeUnit.MILLISECONDS.toNanos(277)))).isEqualTo("17:59:59.277");
     }
-
-    @Test(expected = CoercingSerializeException.class)
+    
+    @Test(expected=CoercingSerializeException.class)
     public void testLocalTimeSerializeInvalidValue() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
 
         //then
         coercing.serialize("");
         coercing.serialize("not a time");
         coercing.serialize(new Object());
-
+        
         fail("Should throw CoercingSerializeException");
-    }
-
+    }    
+    
     @Test
     public void testLocalTimeParseValue() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
 
         //then
         assertThat(coercing.parseValue("00:00:00")).isEqualTo(LocalTime.MIDNIGHT);
@@ -167,31 +168,31 @@ public class JavaScalarsTest {
         assertThat(coercing.parseValue("17:59:59.277")).isEqualTo(LocalTime.of(17, 59, 59, (int) TimeUnit.MILLISECONDS.toNanos(277)));
     }
 
-    @Test(expected = CoercingParseValueException.class)
+    @Test(expected=CoercingParseValueException.class)
     public void testLocalTimeParseValueInvlidValue() {
         //given
-        Coercing<?, ?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
+        Coercing<?,?> coercing = JavaScalars.of(LocalTime.class).getCoercing();
 
         //then
         coercing.parseValue("");
         coercing.parseValue("not a time");
         coercing.parseValue(new Object());
-
+        
         fail("Should throw CoercingParseValueException");
     }
-
+    
     @Test
     public void testNonExistingJavaScalarShouldDefaultToObjectCoercing() {
         //given
         GraphQLScalarType scalarType = JavaScalars.of(VariableValue.class);
 
         //then
-        Coercing<?, ?> coercing = scalarType.getCoercing();
+        Coercing<?,?> coercing = scalarType.getCoercing();
 
         assertThat(coercing).isInstanceOf(GraphQLObjectCoercing.class);
         assertThat(scalarType.getName()).isEqualTo("VariableValue");
     }
-
+    
     @Test
     public void testRegisterJavaScalarWithObjectCoercing() {
         //given
@@ -199,14 +200,13 @@ public class JavaScalarsTest {
 
         //when
         GraphQLScalarType scalarType = JavaScalars.of(Map.class);
-
+        
         //then
-        Coercing<?, ?> coercing = scalarType.getCoercing();
+        Coercing<?,?> coercing = scalarType.getCoercing();
 
         assertThat(coercing).isInstanceOf(GraphQLObjectCoercing.class);
         assertThat(scalarType.getName()).isEqualTo("Map");
     }
-
     @Test
     public void string2OffsetDateTime() {
         //given
@@ -272,7 +272,6 @@ public class JavaScalarsTest {
         assert resultLDT.getYear() == 2019;
         assert resultLDT.getDayOfMonth() == 05;
         assert resultLDT.getMonth() == Month.AUGUST;
-        assert resultLDT.getYear() == 2019;
         assert resultLDT.getHour() == 07;
         assert resultLDT.getMinute() == 15;
         assert resultLDT.getSecond() == 07;
