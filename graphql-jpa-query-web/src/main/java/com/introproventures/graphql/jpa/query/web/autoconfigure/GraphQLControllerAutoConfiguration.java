6de076bc98576d9db8fac4a807631e530782ed50
Igor Dianov <igor@dianov.com>
2019-08-17 03:12:41 -0700

    feat: add conditional property to disable GraphQLController

--- graphql-jpa-query-web/src/main/java/com/introproventures/graphql/jpa/query/web/autoconfigure/GraphQLControllerAutoConfiguration.java
+++ graphql-jpa-query-web/src/main/java/com/introproventures/graphql/jpa/query/web/autoconfigure/GraphQLControllerAutoConfiguration.java
@@ -1,15 +1,18 @@
 package com.introproventures.graphql.jpa.query.web.autoconfigure;
 
-import com.introproventures.graphql.jpa.query.schema.GraphQLExecutor;
-import com.introproventures.graphql.jpa.query.web.GraphQLController;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.introproventures.graphql.jpa.query.schema.GraphQLExecutor;
+import com.introproventures.graphql.jpa.query.web.GraphQLController;
+
 @Configuration
 @ConditionalOnWebApplication
 @ConditionalOnClass(GraphQLExecutor.class)
+@ConditionalOnProperty(name= "spring.graphql.jpa.query.web.enabled", havingValue="true", matchIfMissing=true)
 public class GraphQLControllerAutoConfiguration {
     
     @Import(GraphQLController.class)
---
+++ graphql-jpa-query-web/src/test/java/com/introproventures/graphql/jpa/query/web/autoconfigure/GraphQLControllerAutoConfigurationPropertyDisabledTest.java
@@ -0,0 +1,38 @@
+package com.introproventures.graphql.jpa.query.web.autoconfigure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.introproventures.graphql.jpa.query.schema.GraphQLExecutor;
+import com.introproventures.graphql.jpa.query.web.GraphQLController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT,
+                properties = "spring.graphql.jpa.query.web.enabled=false")
+public class GraphQLControllerAutoConfigurationPropertyDisabledTest {
+    
+    @MockBean
+    private GraphQLExecutor graphQLExecutor;
+    
+    @Autowired(required=false)
+    private GraphQLController graphQLController;
+    
+    @SpringBootApplication
+    static class Application {
+        
+    }
+    
+    @Test
+    public void contextLoads() {
+        assertThat(graphQLController).isNull();
+    }
+
+}
