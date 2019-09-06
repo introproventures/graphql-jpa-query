3ff447380d9081c2409446a75af77f68731e195e
Igor Dianov <igor@dianov.com>
2019-09-05 19:51:00 -0700

    fix: add ArrayUtil test coverage

--- graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/introspection/ArrayUtil.java
+++ graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/introspection/ArrayUtil.java
@@ -6,11 +6,21 @@ public class ArrayUtil {
 
     private static final int INDEX_NOT_FOUND = -1;
 
-    public static boolean isEmpty(Object[] array) {
-        return array == null || array.length == 0;
+    public static boolean isEmpty(Object array) {
+        if(array == null) {
+            return true;
+        }
+        
+        // not an array
+        if(!array.getClass().isArray()) {
+            return false;
+        }
+        
+        // check array length
+        return Array.getLength(array) == 0;
     }
 
-    public static boolean isNotEmpty(Object[] array) {
+    public static boolean isNotEmpty(Object array) {
         return !isEmpty(array);
     }
     
@@ -44,24 +54,6 @@ public class ArrayUtil {
         return INDEX_NOT_FOUND;
     }
 
-//    public static <T> T[] addAll(T[] a, T[] b) {
-//        final int alen = a !=null ? a.length : 0;
-//        final int blen = b != null ? b.length : 0;
-//        if (alen == 0) {
-//            return b;
-//        }
-//        if (blen == 0) {
-//            return a;
-//        }
-//        final T[] result = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), alen + blen);
-//        
-//        System.arraycopy(a, 0, result, 0, alen);
-//        System.arraycopy(b, 0, result, alen, blen);
-//        
-//        return result;
-//    }
-
-    // FIXME
     public static <T> T[] addAll(T[] array1, T[] array2) {
         if (array1 == null) {
             return (T[]) clone(array2);
@@ -90,102 +82,6 @@ public class ArrayUtil {
         return joinedArray;
     }
 
-    public static boolean[] addAll(boolean[] array1, boolean[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        boolean[] joinedArray = new boolean[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-
-    public static char[] addAll(char[] array1, char[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        char[] joinedArray = new char[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-
-    public static byte[] addAll(byte[] array1, byte[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        byte[] joinedArray = new byte[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-
-    public static short[] addAll(short[] array1, short[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        short[] joinedArray = new short[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-
-    public static int[] addAll(int[] array1, int[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        int[] joinedArray = new int[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-
-    public static long[] addAll(long[] array1, long[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        long[] joinedArray = new long[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-
-    public static float[] addAll(float[] array1, float[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        float[] joinedArray = new float[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-
-    public static double[] addAll(double[] array1, double[] array2) {
-        if (array1 == null) {
-            return clone(array2);
-        } else if (array2 == null) {
-            return clone(array1);
-        }
-        double[] joinedArray = new double[array1.length + array2.length];
-        System.arraycopy(array1, 0, joinedArray, 0, array1.length);
-        System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
-        return joinedArray;
-    }
-    
     public static <T> T[] clone(T[] array) {
         if (array == null) {
             return null;
@@ -193,68 +89,5 @@ public class ArrayUtil {
 
         return (T[]) array.clone();
     }
-
-    public static long[] clone(long[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (long[]) array.clone();
-    }
-
-    public static int[] clone(int[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (int[]) array.clone();
-    }
-
-    public static short[] clone(short[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (short[]) array.clone();
-    }
-
-    public static byte[] clone(byte[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (byte[]) array.clone();
-    }
-
-    public static double[] clone(double[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (double[]) array.clone();
-    }
-
-    public static float[] clone(float[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (float[]) array.clone();
-    }
-
-    public static boolean[] clone(boolean[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (boolean[]) array.clone();
-    }
-
-    public static char[] clone(char[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        return (char[]) array.clone();
-    }    
+    
 }
