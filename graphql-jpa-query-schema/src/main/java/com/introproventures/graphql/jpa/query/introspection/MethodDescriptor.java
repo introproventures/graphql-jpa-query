f46ec64e111d0c5b17d6b4d4c73761e9c47f6a9b
Igor Dianov <igor@dianov.com>
2019-09-07 17:04:58 -0700

    fix: add MethodDescriptor tests

--- graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/introspection/MethodDescriptor.java
+++ graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/introspection/MethodDescriptor.java
@@ -3,6 +3,8 @@ package com.introproventures.graphql.jpa.query.introspection;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class MethodDescriptor extends Descriptor implements Getter, Setter {
 
@@ -128,6 +130,29 @@ public class MethodDescriptor extends Descriptor implements Getter, Setter {
     @Override
     public String toString() {
         return classDescriptor.getType().getSimpleName() + '#' + method.getName() + "()";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + Arrays.hashCode(rawParameterTypes);
+        result = prime * result + Objects.hash(method, returnType);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MethodDescriptor other = (MethodDescriptor) obj;
+        return Objects.equals(method, other.method) && Arrays.equals(rawParameterTypes,
+                                                                     other.rawParameterTypes) && Objects.equals(returnType,
+                                                                                                                other.returnType);
     }
 
 }
