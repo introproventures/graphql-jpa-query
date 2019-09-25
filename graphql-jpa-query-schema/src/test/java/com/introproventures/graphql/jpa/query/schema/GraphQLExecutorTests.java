72307317a3e789c52f7b42dd4eb4804ab83a8341
Sinai <sinainin.sn@gmail.com>
2019-09-25 15:24:11 +0700

    #193 add test for error query when total or pages with where condition exists or not_exists

--- graphql-jpa-query-schema/src/test/java/com/introproventures/graphql/jpa/query/schema/GraphQLExecutorTests.java
+++ graphql-jpa-query-schema/src/test/java/com/introproventures/graphql/jpa/query/schema/GraphQLExecutorTests.java
@@ -630,7 +630,7 @@ public class GraphQLExecutorTests {
                 "        title: {LIKE: \"War\"}" + 
                 "      }" + 
                 "    }" + 
-                "  }) {" + 
+                "  }) {" +
                 "    select {" + 
                 "      id" + 
                 "      name" + 
@@ -838,8 +838,80 @@ public class GraphQLExecutorTests {
 
         // then
         assertThat(result.toString()).isEqualTo(expected);
-    }     
-    
+    }
+
+    @Test
+    public void queryTotalForAuthorsWithWhereEXISTSBooksLIKETitleEmpty() {
+        //given
+        String query = "query { "
+                + "Authors(where: {" +
+                "    EXISTS: {" +
+                "      books: {" +
+                "        author: {name: {LIKE: \"Anton\"}}" +
+                "        title: {LIKE: \"War\"}" +
+                "      }" +
+                "    }" +
+                "  }) {" +
+                "    total" +
+                "    select {" +
+                "      id" +
+                "      name" +
+                "      books {" +
+                "        id" +
+                "        title" +
+                "      }" +
+                "    }" +
+                "  }"+
+                "}";
+
+        String expected = "{Authors={select=[]}}";
+
+        //when
+        Object result = executor.execute(query).getData();
+
+        // then
+        assertThat(result.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void queryTotalForAuthorsWithWhereBooksNOTEXISTSAuthorLIKENameLeo() {
+        //given
+        String query = "query { "
+                + "  Authors(where: {" +
+                "    books: {" +
+                "      NOT_EXISTS: {" +
+                "        author: {" +
+                "          name: {LIKE: \"Leo\"}" +
+                "        }" +
+                "      }" +
+                "    }" +
+                "  }) {" +
+                "    total" +
+                "    select {" +
+                "      id" +
+                "      name" +
+                "      books {" +
+                "        id" +
+                "        title" +
+                "      }" +
+                "    }" +
+                "  }"+
+                "}";
+
+        String expected = "{Authors={select=["
+                + "{id=4, name=Anton Chekhov, books=["
+                +   "{id=5, title=The Cherry Orchard}, "
+                +   "{id=6, title=The Seagull}, "
+                +   "{id=7, title=Three Sisters}]}"
+                + "]}}";
+
+        //when
+        Object result = executor.execute(query).getData();
+
+        // then
+        assertThat(result.toString()).isEqualTo(expected);
+    }
+
     @Test
     public void queryForAuthorssWithWhereBooksGenreEquals() {
         //given
