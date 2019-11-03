6a59e799d1d414361720f1bdbf4ab2c24dc7842e
Igor Dianov <igor@dianov.com>
2019-11-03 12:11:21 -0800

    feat(test): added test example for graphql-java-annotations library

--- graphql-jpa-query-autoconfigure/src/test/java/com/introproventures/graphql/jpa/query/autoconfigure/GraphQLSchemaAutoConfigurationTest.java
+++ graphql-jpa-query-autoconfigure/src/test/java/com/introproventures/graphql/jpa/query/autoconfigure/GraphQLSchemaAutoConfigurationTest.java
@@ -1,22 +1,39 @@
 package com.introproventures.graphql.jpa.query.autoconfigure;
 
+import static graphql.annotations.AnnotationsSchemaCreator.newAnnotationsSchema;
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
+
+import com.introproventures.graphql.jpa.query.autoconfigure.support.AdditionalGraphQLType;
+import com.introproventures.graphql.jpa.query.autoconfigure.support.MutationRoot;
+import com.introproventures.graphql.jpa.query.autoconfigure.support.QueryRoot;
+import com.introproventures.graphql.jpa.query.autoconfigure.support.SubscriptionRoot;
 
 import graphql.GraphQL;
 import graphql.Scalars;
+import graphql.annotations.AnnotationsSchemaCreator;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLInvokeDetached;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.directives.definition.GraphQLDirectiveDefinition;
 import graphql.schema.FieldCoordinates;
 import graphql.schema.GraphQLCodeRegistry;
 import graphql.schema.GraphQLFieldDefinition;
@@ -36,7 +53,87 @@ public class GraphQLSchemaAutoConfigurationTest {
     
     @SpringBootApplication
     static class Application {
+        
+        @Configuration
+        static class GraphQLAnnotationsSchemaConfigurer implements GraphQLSchemaConfigurer {
+            @Autowired(required = false)
+            private QueryRoot queryRoot = new NullQuery();
 
+            @Autowired(required = false)
+            private MutationRoot mutationRoot;
+
+            @Autowired(required = false)
+            private SubscriptionRoot subscriptionRoot;
+
+            @Autowired(required = false)
+            private Set<AdditionalGraphQLType> additionalGraphQLTypes;
+
+            @Value("${directives.package:}")
+            private String directivesPackage;
+
+            @Override
+            public void configure(GraphQLShemaRegistration registry) {
+                AnnotationsSchemaCreator.Builder builder = newAnnotationsSchema();
+                builder.query(queryRoot.getClass());
+                if (mutationRoot != null) {
+                    builder.mutation(mutationRoot.getClass());
+                }
+                if (subscriptionRoot != null) {
+                    builder.subscription(subscriptionRoot.getClass());
+                }
+
+                if(!StringUtils.isEmpty(directivesPackage)) {
+                    Reflections reflections = new Reflections(directivesPackage);
+    
+                    Set<Class<?>> directiveDeclarations = reflections.getTypesAnnotatedWith(GraphQLDirectiveDefinition.class);
+    
+                    builder.directives(directiveDeclarations);
+                }
+
+                if (additionalGraphQLTypes!=null) {
+                    builder.additionalTypes(additionalGraphQLTypes.stream()
+                                                                  .map(AdditionalGraphQLType::getClass)
+                                                                  .collect(Collectors.toSet()));
+                }
+
+                registry.register(builder.build());
+            }
+            
+            @GraphQLName("null")
+            class NullQuery implements QueryRoot {
+                
+            }
+        }
+        
+        @Component
+        public static class AnnotatedMutation implements MutationRoot {
+            
+             @GraphQLField
+             @GraphQLInvokeDetached
+             public String salut(@GraphQLName("name") String name) {
+                 return "Salut, " + name + "!";
+             }
+        }
+        
+        @Component
+        public static class AnnotatedQuery implements QueryRoot {
+             @GraphQLField
+             @GraphQLInvokeDetached
+             public Greeting greeting(@GraphQLName("name") String name) {
+                 return new Greeting("Hi, " + name + "!");
+             }
+             
+            public static class Greeting {
+
+                @GraphQLField
+                public String value;
+
+                public Greeting(String value) {
+                    this.value = value;
+                }
+            }
+        }
+        
         @Component
         static class MutationGraphQLSchemaConfigurer implements GraphQLSchemaConfigurer {
 
@@ -81,7 +178,7 @@ public class GraphQLSchemaAutoConfigurationTest {
                                                                                       .dataFetcher(new StaticDataFetcher("world")))
                                                            .field(newFieldDefinition().name("hello2")
                                                                                       .type(Scalars.GraphQLString))
-                                                           .field(newFieldDefinition().name("greetings")
+                                                           .field(newFieldDefinition().name("hello3")
                                                                                       .type(GraphQLObjectType.newObject()
                                                                                                              .name("Hello3")
                                                                                                              .field(newFieldDefinition().name("canada")
@@ -93,7 +190,7 @@ public class GraphQLSchemaAutoConfigurationTest {
                 GraphQLCodeRegistry codeRegistry = GraphQLCodeRegistry.newCodeRegistry()
                                                                       .dataFetcher(FieldCoordinates.coordinates(query.getName(), "hello2"),
                                                                                    new StaticDataFetcher("world2"))
-                                                                      .dataFetcher(FieldCoordinates.coordinates(query.getName(), "greetings"),
+                                                                      .dataFetcher(FieldCoordinates.coordinates(query.getName(), "hello3"),
                                                                                    new StaticDataFetcher(Collections.emptyMap()))
                                                                       .dataFetcher(FieldCoordinates.coordinates("Hello3", "america"),
                                                                                    new StaticDataFetcher("Hi!"))
@@ -113,27 +210,54 @@ public class GraphQLSchemaAutoConfigurationTest {
 
     @Test
     public void contextLoads() {
-        // given
-        GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
-
-        // when
-        Map<String, Object> result = graphQL.execute("query {hello}").getData();
-        Map<String, Object> result2 = graphQL.execute("mutation {greet}").getData();
-        Map<String, Object> result3 = graphQL.execute("mutation {greet2}").getData();
-        Map<String, Object> result4 = graphQL.execute("query {hello2}").getData();
-        Map<String, Object> result5 = graphQL.execute("query { greetings { america canada } }").getData();
-        
-        // then
-        assertThat(result.toString()).isEqualTo("{hello=world}");
-        assertThat(result2.toString()).isEqualTo("{greet=hello world}");
-        assertThat(result3.toString()).isEqualTo("{greet2=hello world2}");
-        assertThat(result4.toString()).isEqualTo("{hello2=world2}");
-        assertThat(result5.toString()).isEqualTo("{greetings={america=Hi!, canada=Eh?}}");
-        
         assertThat(graphQLSchema.getQueryType())
                 .extracting(GraphQLObjectType::getName, GraphQLObjectType::getDescription)
                 .containsExactly("GraphQLBooks", "GraphQL Books Schema Description");
         	
+    }
+    
+    @Test
+    public void querySchemaConfigurer() {
+        // given
+        GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+
+        // when
+        Map<String, Object> query1 = graphQL.execute("query {hello}").getData();
+        Map<String, Object> query2 = graphQL.execute("query {hello2}").getData();
+        Map<String, Object> query3 = graphQL.execute("query {hello3 { america canada } }").getData();
+        
+        // then
+        assertThat(query1.toString()).isEqualTo("{hello=world}");
+        assertThat(query2.toString()).isEqualTo("{hello2=world2}");
+        assertThat(query3.toString()).isEqualTo("{hello3={america=Hi!, canada=Eh?}}");
+    }
+
+    @Test
+    public void mutationSchemaConfigurer() {
+        // given
+        GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+
+        // when
+        Map<String, Object> query1 = graphQL.execute("mutation {greet}").getData();
+        Map<String, Object> query2 = graphQL.execute("mutation {greet2}").getData();
+        
+        // then
+        assertThat(query1.toString()).isEqualTo("{greet=hello world}");
+        assertThat(query2.toString()).isEqualTo("{greet2=hello world2}");
+    }
+
+    @Test
+    public void annotatedSchemaConfigurer() {
+        // given
+        GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+
+        // when
+        Map<String, Object> mutation = graphQL.execute("mutation { salut(name: \"dude\") }").getData();
+        Map<String, Object> query = graphQL.execute("query { greeting(name: \"dude\") { value } }").getData();
+        
+        // then
+        assertThat(mutation.toString()).isEqualTo("{salut=Salut, dude!}");
+        assertThat(query.toString()).isEqualTo("{greeting={value=Hi, dude!}}");
     }
     
 
