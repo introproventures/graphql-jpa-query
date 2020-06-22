9dab5ab8f3f268381771e5cd6947546eb8111400
Igor Dianov <igor@dianov.com>
2020-06-22 16:00:27 -0700

    fix: update Javadoc

--- graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/schema/RestrictedKeysProvider.java
+++ graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/schema/RestrictedKeysProvider.java
@@ -10,13 +10,22 @@ import com.introproventures.graphql.jpa.query.schema.impl.EntityIntrospector.Ent
  * The RestrictedKeysProvider functional interface should provide a list of restricted keys in order to filter records  
  * at runtime based on user security context based on EntityIntrospection descriptor. 
  * 
- * The return argument uses Optional<List<Object>> return type. 
+ * The return argument uses Optional<List<Object>> return type:
  * The non-empty list will restrict the query to provided keys
- * The empty Optional will block running the query and return empty result. 
  * The empty list will run the query unrestricted. 
+ * The empty Optional will block running the query and return empty result. 
  *
  */
 @FunctionalInterface
 public interface RestrictedKeysProvider extends Function<EntityIntrospectionResult, Optional<List<Object>>> {
 
+    /**
+     * Applies this restricted keys provider function to the given argument of entityDescriptor.
+     *
+     * @param entityDescriptor the function argument
+     * @return the function result with optional list of keys
+     */
+    @Override
+    Optional<List<Object>> apply(EntityIntrospectionResult entityDescriptor);
+    
 }
