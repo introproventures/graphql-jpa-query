1b8515a33ffe654a1136db5626e89dd7e1f4ec33
Igor Dianov <igdianov@users.noreply.github.com>
2022-10-25 12:05:01 -0700

    fix: resolve default GraphQLSchemaConfigurer bean race condition in autoconfigure (#312)

--- graphql-jpa-query-autoconfigure/src/main/java/com/introproventures/graphql/jpa/query/autoconfigure/GraphQLSchemaAutoConfiguration.java
+++ graphql-jpa-query-autoconfigure/src/main/java/com/introproventures/graphql/jpa/query/autoconfigure/GraphQLSchemaAutoConfiguration.java
@@ -2,7 +2,7 @@ package com.introproventures.graphql.jpa.query.autoconfigure;
 
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,9 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -22,32 +20,29 @@ import java.util.List;
     @PropertySource("classpath:com/introproventures/graphql/jpa/query/boot/autoconfigure/default.properties"),
     @PropertySource(value = "classpath:graphql-jpa-autoconfigure.properties", ignoreResourceNotFound = true)
 })
+@AutoConfigureAfter(GraphQLSchemaBuilderAutoConfiguration.class)
 public class GraphQLSchemaAutoConfiguration {
 
-    private final List<GraphQLSchemaConfigurer> graphQLSchemaConfigurers = new ArrayList<>();
-    
-    @Autowired(required = true)
-    public void setGraphQLSchemaConfigurers(List<GraphQLSchemaConfigurer> configurers) {
-        if (!CollectionUtils.isEmpty(configurers)) {
-        	graphQLSchemaConfigurers.addAll(configurers);
-        }
+    @Bean
+    @ConditionalOnMissingBean
+    public GraphQLShemaRegistration graphQLShemaRegistration() {
+        return new GraphQLShemaRegistrationImpl();
     }
-    
+
     @Bean
     @ConditionalOnMissingBean(GraphQLSchema.class)
-    public GraphQLSchemaFactoryBean graphQLSchemaFactoryBean(GraphQLJpaQueryProperties properties) {
-    	GraphQLShemaRegistrationImpl graphQLShemaRegistration = new GraphQLShemaRegistrationImpl();
-
+    public GraphQLSchemaFactoryBean graphQLSchemaFactoryBean(GraphQLJpaQueryProperties properties,
+                                                             GraphQLShemaRegistration graphQLShemaRegistration,
+                                                             List<GraphQLSchemaConfigurer> graphQLSchemaConfigurers) {
         for (GraphQLSchemaConfigurer configurer : graphQLSchemaConfigurers) {
             configurer.configure(graphQLShemaRegistration);
         }
-        
+
         return new GraphQLSchemaFactoryBean(graphQLShemaRegistration.getManagedGraphQLSchemas())
 	        		.setQueryName(properties.getName())
 	    			.setQueryDescription(properties.getDescription());
-        
-        
+
+
     };
-    
-    
+
 }
