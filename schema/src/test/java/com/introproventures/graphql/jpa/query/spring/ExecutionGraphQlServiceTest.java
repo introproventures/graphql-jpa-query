733531a8c87cbd1d6d6998f2c7257d09d0479908
Igor Dianov <igor@dianov.com>
2024-01-01 11:40:53 -0800

    add graphQlServiceTesterMap

--- schema/src/test/java/com/introproventures/graphql/jpa/query/spring/ExecutionGraphQlServiceTest.java
+++ schema/src/test/java/com/introproventures/graphql/jpa/query/spring/ExecutionGraphQlServiceTest.java
@@ -1,10 +1,13 @@
 package com.introproventures.graphql.jpa.query.spring;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.introproventures.graphql.jpa.query.AbstractSpringBootTestSupport;
 import com.introproventures.graphql.jpa.query.schema.impl.GraphQLJpaSchemaBuilder;
 import com.introproventures.graphql.jpa.query.schema.model.book.Book;
 import graphql.schema.GraphQLSchema;
 import jakarta.persistence.EntityManager;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +20,12 @@ import org.springframework.graphql.ExecutionGraphQlService;
 import org.springframework.graphql.execution.DefaultExecutionGraphQlService;
 import org.springframework.graphql.execution.GraphQlSource;
 import org.springframework.graphql.test.tester.ExecutionGraphQlServiceTester;
+import org.springframework.graphql.test.tester.GraphQlTester;
 
 @SpringBootTest(properties = "spring.sql.init.data-locations=classpath:books.sql")
 public class ExecutionGraphQlServiceTest extends AbstractSpringBootTestSupport {
 
-    protected static ExecutionGraphQlServiceTester graphQlServiceTester;
+    protected static GraphQlTester graphQlTester;
 
     @SpringBootConfiguration
     @EnableAutoConfiguration
@@ -49,23 +53,42 @@ public class ExecutionGraphQlServiceTest extends AbstractSpringBootTestSupport {
 
     @BeforeAll
     static void configureGraphQlServiceTester(@Autowired ExecutionGraphQlService executionGraphQlService) {
-        graphQlServiceTester = ExecutionGraphQlServiceTester.create(executionGraphQlService);
+        graphQlTester = ExecutionGraphQlServiceTester.create(executionGraphQlService);
     }
 
     @Test
     void graphQlServiceTester() {
-        graphQlServiceTester
+        graphQlTester
             .document("{Books{select{id title}}}")
             .execute()
             .path(".select[*]")
             .matchesJson(
                 """
-                [{"id":2,"title":"War and Peace"},
-                {"id":3,"title":"Anna Karenina"},
-                {"id":5,"title":"The Cherry Orchard"},
-                {"id":6,"title":"The Seagull"},
-                {"id":7,"title":"Three Sisters"}]
+                [
+                    {"id":2,"title":"War and Peace"},
+                    {"id":3,"title":"Anna Karenina"},
+                    {"id":5,"title":"The Cherry Orchard"},
+                    {"id":6,"title":"The Seagull"},
+                    {"id":7,"title":"Three Sisters"}
+                ]
                 """
             );
+    }
+
+    @Test
+    void graphQlServiceTesterMap() {
+        var query = "{Books {select {id title}}}";
+
+        var expected =
+            "[{id=2, title=War and Peace}, {id=3, title=Anna Karenina}, {id=5, title=The Cherry Orchard}, {id=6, title=The Seagull}, {id=7, title=Three Sisters}]";
+
+        graphQlTester
+            .document(query)
+            .execute()
+            .errors()
+            .verify()
+            .path(".select[*]")
+            .entityList(Map.class)
+            .satisfies(result -> assertThat(result.toString()).isEqualTo(expected));
     }
 }
