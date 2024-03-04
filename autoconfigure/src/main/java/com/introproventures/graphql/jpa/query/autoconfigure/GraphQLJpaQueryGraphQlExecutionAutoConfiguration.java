4eae7adee9c56f9aa4c9ccfdb5958708565f154c
Igor Dianov <igor@dianov.com>
2024-03-04 11:54:41 -0800

    fix duplicate dataloader error

--- autoconfigure/src/main/java/com/introproventures/graphql/jpa/query/autoconfigure/GraphQLJpaQueryGraphQlExecutionAutoConfiguration.java
+++ autoconfigure/src/main/java/com/introproventures/graphql/jpa/query/autoconfigure/GraphQLJpaQueryGraphQlExecutionAutoConfiguration.java
@@ -3,6 +3,8 @@ package com.introproventures.graphql.jpa.query.autoconfigure;
 import static com.introproventures.graphql.jpa.query.schema.impl.BatchLoaderRegistry.getMappedBatchDataLoaderMap;
 
 import graphql.GraphQL;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import org.dataloader.DataLoaderOptions;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.ListableBeanFactory;
@@ -14,7 +16,6 @@ import org.springframework.boot.autoconfigure.graphql.GraphQlAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.graphql.ExecutionGraphQlService;
 import org.springframework.graphql.execution.BatchLoaderRegistry;
-import org.springframework.graphql.execution.DefaultExecutionGraphQlService;
 import org.springframework.graphql.execution.GraphQlSource;
 import reactor.core.publisher.Mono;
 
@@ -25,7 +26,18 @@ public class GraphQLJpaQueryGraphQlExecutionAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     BatchLoaderRegistry batchLoaderRegistry(ListableBeanFactory beanFactory) {
-        return new GraphQlAutoConfiguration(beanFactory).batchLoaderRegistry();
+        var batchLoaderRegistry = new GraphQlAutoConfiguration(beanFactory).batchLoaderRegistry();
+
+        DataLoaderOptions options = DataLoaderOptions.newOptions().setCachingEnabled(false);
+
+        batchLoaderRegistry
+            .forName(GraphQLJpaQueryGraphQlExecutionAutoConfiguration.class.getName())
+            .withOptions(options)
+            .registerMappedBatchLoader((keys, env) ->
+                Mono.fromCompletionStage(CompletableFuture.completedStage(Map.of()))
+            );
+
+        return batchLoaderRegistry;
     }
 
     @Bean
@@ -40,10 +52,7 @@ public class GraphQLJpaQueryGraphQlExecutionAutoConfiguration {
     }
 
     @Bean
-    InitializingBean batchLoaderRegistryConfigurer(
-        DefaultExecutionGraphQlService executionGraphQlService,
-        BatchLoaderRegistry batchLoaderRegistry
-    ) {
+    InitializingBean batchLoaderRegistryConfigurer(BatchLoaderRegistry batchLoaderRegistry) {
         return () -> {
             DataLoaderOptions options = DataLoaderOptions.newOptions().setCachingEnabled(false);
 
@@ -56,8 +65,6 @@ public class GraphQLJpaQueryGraphQlExecutionAutoConfiguration {
                             Mono.fromCompletionStage(mappedBatchLoader.load(keys, env))
                         )
                 );
-
-            executionGraphQlService.addDataLoaderRegistrar(batchLoaderRegistry);
         };
     }
 }
