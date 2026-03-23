0f64e44d8e83602e03b4d26570fcdf9892fa42c0
Igor Dianov <igor@dianov.com>
2026-03-23 06:29:46 -0700

    Update Postgres testcontainer image to 16-alpine

--- tests/gatling/src/test/java/com/introproventures/graphql/jpa/query/example/TestApplication.java
+++ tests/gatling/src/test/java/com/introproventures/graphql/jpa/query/example/TestApplication.java
@@ -13,7 +13,7 @@ public class TestApplication {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
     }
 
     public static void main(String[] args) {
